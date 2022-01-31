c929b6fc5dcdb42d762e5297723f8e9dd210df60
Vladimir Ioshkin <bobbydigital92@gmail.com>
2022-01-31 23:25:33 +0300

    fix text

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -17,9 +17,11 @@ const startEvenGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
+
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     if (getRandom() === false) {
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
